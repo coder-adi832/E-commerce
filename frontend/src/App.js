9f1ac0b03865763255fab1012772eea67970d32d
Aditya Gupta <adg17022002@gmail.com>
2025-05-30 12:30:42 +0530

    Day 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,11 @@ import Product from './Pages/Product';
 import LoginSignUp from './Pages/LoginSignUp';
 import Cart from './Pages/Cart';
 import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/Frontend_Assets/banner_mens.png'
+import women_banner from './Components/Assets/Frontend_Assets/banner_women.png'
+import kids_banner from './Components/Assets/Frontend_Assets/banner_kids.png'
+
+
 function App() {
   return (
     <div>
@@ -15,9 +20,9 @@ function App() {
         <Navbar/>
           <Routes>
             <Route path='/' element = {<Shop></Shop>}/>
-            <Route path='/mens' element = {<ShopCategory category = "mens"/>}/>
-            <Route path='/womens' element = {<ShopCategory category = "womens"/>}/>
-            <Route path='/kids' element = {<ShopCategory category = "kids"/>}/>
+            <Route path='/men' element = {<ShopCategory banner = {men_banner} category = "men"/>}/>
+            <Route path='/women' element = {<ShopCategory banner = {women_banner} category = "women"/>}/>
+            <Route path='/kids' element = {<ShopCategory banner = {kids_banner} category = "kid"/>}/>
             <Route path='product' element = {<Product/>}>
               <Route path=':productID' element = {<Product/>}/>
             </Route>
---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-navbox">
+                Description
+            </div>
+            <div className="descriptionbox-navbox fade">
+                Reviews (122)
+            </div>
+        </div>
+        <div className="descriptionbox-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum odio necessitatibus iusto deserunt porro, provident sint, autem ducimus aut atque illo quibusdam ex? Voluptatum suscipit dicta voluptatem eaque eos quidem autem ullam unde possimus corporis assumenda veniam quos, iste, aut, corrupti facere saepe exercitationem quibusdam nesciunt nihil dignissimos? Nihil, enim.
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
